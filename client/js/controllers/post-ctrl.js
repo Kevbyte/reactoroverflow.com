ff7da3ceabc315b99ba61aab8bcaee75e899af59
Kevbyte <kevin.schweigert@gmail.com>
2015-08-07 13:53:21 -0700

    started work on post view

--- client/js/controllers/post-ctrl.js
+++ client/js/controllers/post-ctrl.js
@@ -1,8 +1,19 @@
+'use strict';
 /**
  * Post Controller
  */
 
 angular.module('RDash')
 .controller('PostCtrl', function PostCtrl($scope) {
-  $scope.
+  $scope.data = {
+    posts: [
+      {
+        title: 'how to make a grown man cry',
+        author: 'Andrew Koshinoko',
+        content: 'first you need to push them onto the ground and then...',
+        tags: ['#cry', '#cruelty']
+      }
+    ]
+  };
+
 });
