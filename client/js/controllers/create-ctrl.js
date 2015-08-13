8f1f0807377f8bc16b9ebcd9b81e09538865522c
Kevbyte <kevin.schweigert@gmail.com>
2015-08-13 08:37:06 -0700

    file upload done

--- client/js/controllers/create-ctrl.js
+++ client/js/controllers/create-ctrl.js
@@ -5,16 +5,30 @@
  */
 
 angular.module('RDash')
-.controller('CreateCtrl', function CreateCtrl ($scope, $location, Posts) {
+.controller('CreateCtrl', function CreateCtrl ($scope, $location, $q,Posts) {
+  var anotherDeferred = $q.defer();
   $scope.title = '';
   $scope.content = '';
   $scope.tags = [];
+  $scope.data = '';
+  $scope.add = function() {
+    var preview = document.getElementById('pic');
+    var f = document.getElementById('file').files[0];
+    var r = new FileReader();
+    r.onloadend = function(e){
+      preview.src = e.target.result;
+      $scope.data = e.target.result;
+    }
+    r.readAsDataURL( f );
+  }; //adds image data to
   $scope.createPost = function() {
     $scope.post = {
       title: $scope.title, 
       content: $scope.content, 
-      tags: $scope.tags
+      tags: $scope.tags,
+      data: $scope.data
       }; //keys: title, content and tags
+    console.log('Inside createPost(), $scope.post = ', $scope.post);
     Posts.addPost($scope.post)
     .then(function(resp) {
       $location.path('/post/'+resp._id); //takes user to the post they created.
